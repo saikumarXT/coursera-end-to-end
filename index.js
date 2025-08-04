0fed8b5a1f48ca28e514496d512201df52119c85
Sai Patel <saikumaraggu6@gmail.com>
2025-08-05 05:02:20 +0530

    created skelton routes

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ const JWT=require('jsonwebtoken');
 const mongoose=require('mongoose');
 
 
+
 const app=express();
 
-app.post('/signup',async function())
+app.post('/user/signup',async function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+await UserModel.create({
+    username:username,
+    password:password })
+})
+
+
+app.post('/user/signin',async function(req,res){
+const username=req.body.username;
+const password=req.body.password;
+
+res.json({
+    message:"signin endpoint"
+   })
+})
+
+app.post("/user/purchases",async function(req,res){
+    res.json({
+        message:'purchase'
+    })
+})
+
+
+app.post("/user/purchase",async function(req,res){
+    res.json({
+        message:'purchase'
+    })
+})
+
+
+app.post("/courses",async function(req,res){
+    res.json({
+        message:'purchase'
+    })
+})
